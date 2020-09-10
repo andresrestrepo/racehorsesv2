36907dfecf4010429d11dba73bae9eb7feaac5e5
andresrestrepo <andresr.alz@gmail.com>
2020-09-09 22:03:44 -0500

    Updates

---
+++ precache-manifest.00b52ddf992a8c111546a011dbb6af67.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283f807494371441edacf612c499f3f1",
+    "url": "/racehorsesv2/index.html"
+  },
+  {
+    "revision": "9b5699ae2a5aa555da1f",
+    "url": "/racehorsesv2/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "74c0fc3768a35800cd86",
+    "url": "/racehorsesv2/static/css/main.a9e8d8c8.chunk.css"
+  },
+  {
+    "revision": "9b5699ae2a5aa555da1f",
+    "url": "/racehorsesv2/static/js/2.491fac0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/racehorsesv2/static/js/2.491fac0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74c0fc3768a35800cd86",
+    "url": "/racehorsesv2/static/js/main.a4d4731a.chunk.js"
+  },
+  {
+    "revision": "047f2957f41a2f176cf2",
+    "url": "/racehorsesv2/static/js/runtime-main.f259781b.js"
+  },
+  {
+    "revision": "2abb93f389f9ea8a3e2af96ebf9c2922",
+    "url": "/racehorsesv2/static/media/horse-static.2abb93f3.png"
+  },
+  {
+    "revision": "b7461258ad0edda4b3549fb609a2394f",
+    "url": "/racehorsesv2/static/media/horse.b7461258.gif"
+  }
+]);
